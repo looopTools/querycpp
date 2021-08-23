3e3132a599bd9b2c3e3c1dc475770933145cbc8b
Lars Nielsen <lnc13.lars@gmail.com>
2021-08-23 11:12:01 +0200

    [:hammer: #13] Adding testing type_to_sql_str for all numverical types types

--- test/test_helpers/test_helpers.cpp
+++ test/test_helpers/test_helpers.cpp
@@ -212,8 +212,224 @@ TEST(test_helpers, test_type_to_sql_str_int)
     unsigned int e = 74;
     EXPECTED = "74";
     EXPECT_EQ(querycpp::helpers::type_to_sql_str(e), EXPECTED);            
-
 }
+
+TEST(test_querycpp_helpers, test_type_to_sql_str_long)
+{
+    std::string EXPECTED;
+    
+    long a = 1992;
+    EXPECTED = "1992";
+    EXPECT_EQ(querycpp::helpers::type_to_sql_str(a), EXPECTED);
+
+    long int b = 890;
+    EXPECTED = "890";
+    EXPECT_EQ(querycpp::helpers::type_to_sql_str(b), EXPECTED);
+    
+    signed long c = 420; 
+    EXPECTED = "420";
+    EXPECT_EQ(querycpp::helpers::type_to_sql_str(c), EXPECTED);
+
+    signed long int d = 1337;
+    EXPECTED = "1337";
+    EXPECT_EQ(querycpp::helpers::type_to_sql_str(d), EXPECTED);
+
+    unsigned long e = 42;
+    EXPECTED = "42";
+    EXPECT_EQ(querycpp::helpers::type_to_sql_str(e), EXPECTED);    
+
+    unsigned long int f = 42000;
+    EXPECTED = "42000";
+    EXPECT_EQ(querycpp::helpers::type_to_sql_str(f), EXPECTED);
+
+    signed long long g = 8;
+    EXPECTED = "8";
+    EXPECT_EQ(querycpp::helpers::type_to_sql_str(g), EXPECTED);    
+
+    unsigned long long h = 9000;
+    EXPECTED = "9000";
+    EXPECT_EQ(querycpp::helpers::type_to_sql_str(h), EXPECTED);
+
+    unsigned long long int i = 8200;
+    EXPECTED = "8200";
+    EXPECT_EQ(querycpp::helpers::type_to_sql_str(i), EXPECTED);    
+}
+
+TEST(test_querycpp_helpers, test_type_to_sql_str_short)
+{
+    std::string EXPECTED;
+    
+    short a = 32;
+    EXPECTED = "32";
+    EXPECT_EQ(querycpp::helpers::type_to_sql_str(a), EXPECTED);
+
+    short int b = 42;
+    EXPECTED = "42";
+    EXPECT_EQ(querycpp::helpers::type_to_sql_str(b), EXPECTED);
+
+    signed short c = 19;
+    EXPECTED = "19";
+    EXPECT_EQ(querycpp::helpers::type_to_sql_str(c), EXPECTED);    
+
+    signed short int d = 20;
+    EXPECTED = "20";
+    EXPECT_EQ(querycpp::helpers::type_to_sql_str(d), EXPECTED);
+
+    unsigned short e = 10;
+    EXPECTED = "10";
+    EXPECT_EQ(querycpp::helpers::type_to_sql_str(e), EXPECTED);    
+
+    unsigned short int f = 5;
+    EXPECTED = "5";
+    EXPECT_EQ(querycpp::helpers::type_to_sql_str(f), EXPECTED);
+}
+
+TEST(test_querycpp_helpers, test_type_to_sql_str_floating_point)
+{
+    std::string EXPECTED;
+    
+    float a = 5.0;
+    EXPECTED = std::to_string(a);
+    EXPECT_EQ(querycpp::helpers::type_to_sql_str(a), EXPECTED);
+
+    double b = 137.85;
+    EXPECTED = std::to_string(b);
+    EXPECT_EQ(querycpp::helpers::type_to_sql_str(b), EXPECTED);
+
+    long double c = 1337.666;
+    EXPECTED = std::to_string(c);
+    EXPECT_EQ(querycpp::helpers::type_to_sql_str(c), EXPECTED);    
+}
+
+TEST(test_querycpp_helpers, test_type_to_sql_str_uint_N)
+{
+    std::string EXPECTED;
+
+    uint8_t a = 16;
+    EXPECTED = "16";
+    EXPECT_EQ(querycpp::helpers::type_to_sql_str(a), EXPECTED);
+
+    
+    uint16_t b = 32;
+    EXPECTED = "32";
+    EXPECT_EQ(querycpp::helpers::type_to_sql_str(b), EXPECTED);
+    
+    uint32_t c = 64;
+    EXPECTED = "64";
+    EXPECT_EQ(querycpp::helpers::type_to_sql_str(c), EXPECTED);
+    
+    uint64_t d = 128;
+    EXPECTED = "128";
+    EXPECT_EQ(querycpp::helpers::type_to_sql_str(d), EXPECTED);
+    
+    uint_fast8_t e = 16;
+    EXPECTED = "16";
+    EXPECT_EQ(querycpp::helpers::type_to_sql_str(e), EXPECTED);
+    
+    uint_fast16_t f = 32;
+    EXPECTED = "32";
+    EXPECT_EQ(querycpp::helpers::type_to_sql_str(f), EXPECTED);
+    
+    uint_fast32_t g = 64;
+    EXPECTED = "64";
+    EXPECT_EQ(querycpp::helpers::type_to_sql_str(g), EXPECTED);
+    
+    uint_fast64_t h = 128;
+    EXPECTED = "128";
+    EXPECT_EQ(querycpp::helpers::type_to_sql_str(h), EXPECTED);
+    
+    uint_least8_t i = 16;
+    EXPECTED = "16";
+    EXPECT_EQ(querycpp::helpers::type_to_sql_str(i), EXPECTED);
+    
+    uint_least16_t j = 32;
+    EXPECTED = "32";
+    EXPECT_EQ(querycpp::helpers::type_to_sql_str(j), EXPECTED);
+    
+    uint_least32_t k = 64;
+    EXPECTED = "64";
+    EXPECT_EQ(querycpp::helpers::type_to_sql_str(k), EXPECTED);
+    
+    uint_least64_t l = 128;
+    EXPECTED = "128";
+    EXPECT_EQ(querycpp::helpers::type_to_sql_str(l), EXPECTED);
+
+    uintmax_t m = 666;
+    EXPECTED = "666";
+    EXPECT_EQ(querycpp::helpers::type_to_sql_str(m), EXPECTED);
+
+    uintptr_t n = m;
+    EXPECTED = "666";
+    EXPECT_EQ(querycpp::helpers::type_to_sql_str(n), EXPECTED);
+
+    size_t o = 1337;
+    EXPECTED = "1337";
+    EXPECT_EQ(querycpp::helpers::type_to_sql_str(o), EXPECTED);
+}
+    
+TEST(test_querycpp_helpers, test_type_to_sql_str_int_N)
+{
+    std::string EXPECTED;
+    int8_t a = 16;
+    EXPECTED = "16";
+    EXPECT_EQ(querycpp::helpers::type_to_sql_str(a), EXPECTED);
+
+    
+    int16_t b = 32;
+    EXPECTED = "32";
+    EXPECT_EQ(querycpp::helpers::type_to_sql_str(b), EXPECTED);
+    
+    int32_t c = 64;
+    EXPECTED = "64";
+    EXPECT_EQ(querycpp::helpers::type_to_sql_str(c), EXPECTED);
+    
+    int64_t d = 128;
+    EXPECTED = "128";
+    EXPECT_EQ(querycpp::helpers::type_to_sql_str(d), EXPECTED);
+    
+    int_fast8_t e = 16;
+    EXPECTED = "16";
+    EXPECT_EQ(querycpp::helpers::type_to_sql_str(e), EXPECTED);
+    
+    int_fast16_t f = 32;
+    EXPECTED = "32";
+    EXPECT_EQ(querycpp::helpers::type_to_sql_str(f), EXPECTED);
+    
+    int_fast32_t g = 64;
+    EXPECTED = "64";
+    EXPECT_EQ(querycpp::helpers::type_to_sql_str(g), EXPECTED);
+    
+    int_fast64_t h = 128;
+    EXPECTED = "128";
+    EXPECT_EQ(querycpp::helpers::type_to_sql_str(h), EXPECTED);
+    
+    int_least8_t i = 16;
+    EXPECTED = "16";
+    EXPECT_EQ(querycpp::helpers::type_to_sql_str(i), EXPECTED);
+    
+    int_least16_t j = 32;
+    EXPECTED = "32";
+    EXPECT_EQ(querycpp::helpers::type_to_sql_str(j), EXPECTED);
+    
+    int_least32_t k = 64;
+    EXPECTED = "64";
+    EXPECT_EQ(querycpp::helpers::type_to_sql_str(k), EXPECTED);
+    
+    int_least64_t l = 128;
+    EXPECTED = "128";
+    EXPECT_EQ(querycpp::helpers::type_to_sql_str(l), EXPECTED);
+
+    intmax_t m = 666;
+    EXPECTED = "666";
+    EXPECT_EQ(querycpp::helpers::type_to_sql_str(m), EXPECTED);
+
+    intptr_t n = m;
+    EXPECTED = "666";
+    EXPECT_EQ(querycpp::helpers::type_to_sql_str(n), EXPECTED);    
+}
+
+
+
 
 int main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);
